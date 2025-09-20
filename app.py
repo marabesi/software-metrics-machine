34a850ef9f30017fa5535c8726ba39a77dadd446
marabesi <matheus.marabesi@gmail.com>
2025-09-20 21:05:33 +0200

    feat(github): dashboard with tabs

--- app.py
+++ app.py
@@ -20,47 +20,81 @@ start_date_picker = pn.widgets.DatePicker(name="Start Date", value=date(2024, 4,
 end_date_picker = pn.widgets.DatePicker(name="End Date", value=date(2025, 9, 30))
 
 
+# Pipeline Section
 def plot_workflow_by_status(start_date, end_date):
     # if start_date and end_date:
     #     print(f"Filtering Workflow By Status data from {start_date} to {end_date}")
     return ViewWorkflowByStatus().main(start_date=start_date, end_date=end_date)
 
 
-def plot_average_prs_open_by():
+pipeline_section = pn.Column(
+    "## Pipeline Section",
+    start_date_picker,
+    end_date_picker,
+    pn.bind(
+        plot_workflow_by_status, start_date=start_date_picker, end_date=end_date_picker
+    ),
+)
+
+
+# PRs Section
+def plot_average_prs_open_by(start_date, end_date):
+    # if start_date and end_date:
+    #     print(f"Filtering Average PRs Open By data from {start_date} to {end_date}")
     return ViewAverageOfPrsOpenBy().main()
 
 
-def plot_review_time_by_author():
-    return ViewAverageReviewTimeByAuthor().plot_average_open_time(title="test")
+def plot_average_review_time_by_author(start_date, end_date):
+    # if start_date and end_date:
+    #     print(f"Filtering Average Review Time By Author data from {start_date} to {end_date}")
+    return ViewAverageReviewTimeByAuthor().plot_average_open_time(title="testtts")
 
 
-def prs_by_author():
-    return ViewPrsByAuthor().plot_top_authors(title="test", top=10)
+def plot_prs_by_author(start_date, end_date):
+    # if start_date and end_date:
+    #     print(f"Filtering PRs By Author data from {start_date} to {end_date}")
+    return ViewPrsByAuthor().plot_top_authors("asdasdassa")
 
 
-dashboard = pn.Column(
-    "## Charts Viewer with Date Picker",
+prs_section = pn.Column(
+    "## PRs Section",
     start_date_picker,
     end_date_picker,
     pn.Row(
         pn.Column(
-            "### Workflow By Status",
+            "### Average PRs Open By",
             pn.bind(
-                plot_workflow_by_status,
+                plot_average_prs_open_by,
+                start_date=start_date_picker,
+                end_date=end_date_picker,
+            ),
+        )
+    ),
+    pn.Row(
+        pn.Column(
+            "### Average Review Time By Author",
+            pn.bind(
+                plot_average_review_time_by_author,
+                start_date=start_date_picker,
+                end_date=end_date_picker,
+            ),
+        )
+    ),
+    pn.Row(
+        pn.Column(
+            "### PRs By Author",
+            pn.bind(
+                plot_prs_by_author,
                 start_date=start_date_picker,
                 end_date=end_date_picker,
             ),
         ),
     ),
-    pn.Row(
-        pn.Column("### Average PRs Open By", pn.bind(plot_average_prs_open_by)),
-    ),
-    pn.Row(
-        pn.Column("### An By", pn.bind(plot_review_time_by_author)),
-    ),
-    pn.Row(
-        pn.Column("### xxxx By", pn.bind(prs_by_author)),
-    ),
+)
+
+dashboard = pn.Tabs(
+    ("Pipeline", pipeline_section),
+    ("PRs", prs_section),
 )
 
 dashboard.servable()
