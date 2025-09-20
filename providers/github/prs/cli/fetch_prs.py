1e593aeecf2a4f4739ef49eeca1ac1be4da2e6b4
marabesi <matheus.marabesi@gmail.com>
2025-09-20 19:27:09 +0200

    fix(strcture): arg names

--- providers/github/prs/cli/fetch_prs.py
+++ providers/github/prs/cli/fetch_prs.py
@@ -19,9 +19,9 @@ from infrastructure.configuration import Configuration
     default=False,
     help="Force re-fetching PRs even if already fetched",
 )
-def execute(months_back=1, cutoff_date=None, force=None):
+def execute(months=1, cutoff_date=None, force=None):
     client = GithubClient(configuration=Configuration())
-    client.fetch_prs(months_back=months_back, cutoff_date=cutoff_date, force=force)
+    client.fetch_prs(months_back=months, cutoff_date=cutoff_date, force=force)
 
 
 command = execute
