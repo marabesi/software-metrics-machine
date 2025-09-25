d8cc97af5775c82792c50f90be91ee0d17a0a885
marabesi <matheus.marabesi@gmail.com>
2025-09-25 08:05:11 +0200

    wip: automated tests with real colaborators

--- run-test.sh
+++ run-test.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-echo "Cleaning up..."
-rm -rf ./data.csv
+# Function to unify rm -rf operations
+cleanup() {
+  rm -rf "$(pwd)/tmp_test"
+  rm -rf "$(pwd)/data.csv"
+}
+
+cleanup
+mkdir -p "$(pwd)/tmp_test/repo"
+
+cp -R "$(pwd)/fixtures/github" "$(pwd)/tmp_test"
+
+export SSM_STORE_DATA_AT="$(pwd)/tmp_test/github"
+export SSM_GIT_REPOSITORY_LOCATION="$(pwd)/tmp_test/repo"
+export SSM_GITHUB_REPOSITORY="fake/repo-test"
 
 echo "Running tests..."
 export PYTHONPATH="$(pwd):$PYTHONPATH"
 poetry run pytest -s "$@"
 
 echo "Cleaning up..."
-rm -rf ./data.csv
+cleanup
 
 echo "Done"
