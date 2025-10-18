e445f7431c39e344fa30ac236e0d27b04c962e77
marabesi <2129872+marabesi@users.noreply.github.com>
2025-10-18 21:11:49 +0200

    chore(core): renamed workflow to pipeline

--- providers/github/workflows/cli/workflow_runs_by.py
+++ providers/github/workflows/cli/workflow_runs_by.py
@@ -5,7 +5,7 @@ from core.infrastructure.configuration.configuration_builder import (
     Driver,
 )
 from core.pipelines.pipelines_repository import PipelinesRepository
-from core.pipelines.plots.view_workflow_runs_by_week_or_month import (
+from core.pipelines.plots.view_pipeline_runs_by_week_or_month import (
     ViewWorkflowRunsByWeekOrMonth,
 )
 
