a2792160817854ab407ab4eb4b5d3f3f42719c4f
marabesi <2129872+marabesi@users.noreply.github.com>
2025-11-01 09:25:06 +0100

    chore(cli): split test files

--- tests/acceptance/test_cli_code.py
+++ tests/acceptance/test_cli_code.py
@@ -84,60 +84,6 @@ class TestCliCodeCommands:
                 "false",
             ] == run_command
 
-    def test_can_run_code_churn_without_data_available(self, cli):
-        result = cli.runner.invoke(
-            main,
-            [
-                "code",
-                "code-churn",
-                "--out-file",
-                "code_churn.png",
-                "--start-date",
-                "2025-01-01",
-                "--end-date",
-                "2025-01-01",
-            ],
-        )
-        assert "No code churn data available to plot" in result.output
-        assert result.stderr == ""
-
-    @pytest.mark.skip(
-        reason="Needs refactoring to return PlotResult from render methods"
-    )
-    def test_given_the_data_it_renders_code_churn(self, cli):
-        path_string = cli.data_stored_at
-        csv_data = """date,added,deleted,commits
-2022-06-17,10,10,2"""
-        FileHandlerForTesting(path_string).store_file("abs-churn.csv", csv_data)
-
-        result = cli.runner.invoke(
-            main,
-            ["code", "code-churn", "--out-file", "code_churn.png"],
-        )
-        assert f"Saved plot to {path_string}/code_churn.png" in result.output
-
-    def test_renders_code_churn_by_date(self, cli):
-        path_string = cli.data_stored_at
-
-        csv_data = """date,added,deleted,commits
-2022-06-17,10,10,2"""
-        FileHandlerForTesting(path_string).store_file("abs-churn.csv", csv_data)
-
-        result = cli.runner.invoke(
-            main,
-            [
-                "code",
-                "code-churn",
-                "--start-date",
-                "2025-01-01",
-                "--end-date",
-                "2025-01-01",
-                "--out-file",
-                "code_churn.png",
-            ],
-        )
-        assert "No code churn data available to plot" in result.output
-
     def test_can_run_entity_churn_without_data_available(self, cli):
         result = cli.runner.invoke(
             main,
