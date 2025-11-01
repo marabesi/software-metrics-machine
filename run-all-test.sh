5f7ab8b34b6744f23cf99a03b10aa6c278240e15
marabesi <2129872+marabesi@users.noreply.github.com>
2025-11-01 07:19:15 +0100

    chore: adds run-all-test script

---
+++ run-all-test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Function to unify rm -rf operations
+cleanup() {
+  rm -rf "$(pwd)/tmp_test"
+  rm -rf "$(pwd)/data.csv"
+}
+
+cleanup
+mkdir -p "$(pwd)/tmp_test/repo"
+
+cp -R "$(pwd)/fixtures/github" "$(pwd)/tmp_test"
+
+export SMM_STORE_DATA_AT="$(pwd)/tmp_test/github"
+
+TEMPLATE=$(cat <<EOF
+{
+  "git_provider": "github",
+  "github_token": "fake-token",
+  "github_repository": "fake-repo-test",
+  "git_repository_location": "$(pwd)/tmp_test/repo",
+  "deployment_frequency_target_pipeline": "",
+  "deployment_frequency_target_job": ""
+}
+EOF
+)
+
+echo "$TEMPLATE" > "$(pwd)/tmp_test/github/smm_config.json"
+
+echo "Running tests..."
+export PYTHONPATH="$(pwd):$PYTHONPATH"
+poetry run pytest -s "$@"
+
+echo "Cleaning up..."
+cleanup
+
+echo "Done"
+
+
+echo "Running e2e"
+
+./lib/bashunit tests/e2e
+
+echo "Done"
