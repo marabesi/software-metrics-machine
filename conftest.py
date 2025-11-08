4701784d0314267ee99a536cc7e443c884c1c38b
marabesi <2129872+marabesi@users.noreply.github.com>
2025-11-08 16:36:32 +0100

    ci: python path

--- conftest.py
+++ conftest.py
@@ -6,10 +6,10 @@ import pytest
 import sys
 from click.testing import CliRunner
 
-from software_metrics_machine.core.infrastructure.configuration.configuration import (
+from src.software_metrics_machine.core.infrastructure.configuration.configuration import (
     Configuration,
 )
-from software_metrics_machine.core.infrastructure.configuration.configuration_file_system_handler import (
+from src.software_metrics_machine.core.infrastructure.configuration.configuration_file_system_handler import (
     ConfigurationFileSystemHandler,
 )
 from tests.in_memory_configuration import InMemoryConfiguration
