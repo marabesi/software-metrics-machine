7f1a32859ca961c53d3f720f1bbf70666ab05823
marabesi <2129872+marabesi@users.noreply.github.com>
2025-12-14 18:03:34 +0100

    chore: updated pandas params

--- src/software_metrics_machine/core/infrastructure/pandas.py
+++ src/software_metrics_machine/core/infrastructure/pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
 pd.set_option("display.max_seq_items", None)
 pd.set_option("display.width", None)
+pd.set_option("display.max_colwidth", None)
