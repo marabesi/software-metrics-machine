a4691f43f46297bc0e4615edf998be501f0a8d5f
marabesi <2129872+marabesi@users.noreply.github.com>
2025-12-26 09:55:12 +0100

    chore: removes playright dep

--- tests/apps/dashboard/test_dashboard_insights.py
+++
@@ -1,6 +0,0 @@
-class TestDashboardInsights:
-
-    def test_plot_insights(self, page):
-        url = "http://localhost:5006"
-        page.goto(url)
-        page.get_by_text("Insight section").wait_for()
